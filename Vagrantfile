1cf47b53703c4f9d4c1ae60a6d6d6d95515000af
Olivier Adam <olivieradam666@gmail.com>
2015-02-10 22:28:16 +0100

    Fix windows run.bat

--- Vagrantfile
+++ Vagrantfile
@@ -9,7 +9,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box_url = "https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
   config.vm.network "private_network", ip: "192.168.50.60"
 
-  config.vm.synced_folder "../proprio-data", "/proprio-data", create: true, owner: "proprio"
+  config.vm.synced_folder "../proprio-data", "/proprio-data", create: true
   config.vm.provision "shell" do |shell|
     shell.path = 'vagrant-provision.sh'
   end
--- vagrant-provision.sh
+++ vagrant-provision.sh
@@ -2,12 +2,14 @@
 curl -sSL https://get.docker.com/ubuntu/ | sh
 gpasswd -a vagrant docker #sudo-less docker
 
-#install utilities
-apt-get install -y curl
+#stop the app
+docker stop $(docker ps -q)
 
 #generate secret key
 SECRET_KEY="$(docker run oadam/proprio 2>&1|sed -n '23,23 p')"
 echo will use secret key: $SECRET_KEY
 
 # start the app
-docker run -d -p 80:8000 -e SECRET_KEY=$SECRET_KEY -v /proprio-data:/data oadam/proprio
+echo -e "\n\nproprio will start now. Once started it will be available on http://192.168.50.60/"
+docker run -p 80:8000 -e SECRET_KEY=$SECRET_KEY -e -v /proprio-data:/data oadam/proprio
+
