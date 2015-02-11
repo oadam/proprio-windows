a9541389832ee8d16d37cb87dbb1683e216b5eb8
Olivier Adam <olivier.adam@m4x.org>
2015-02-11 09:38:58 +0100

    Add quotes around $SECRET_KEY

--- vagrant-provision.sh
+++ vagrant-provision.sh
@@ -10,9 +10,9 @@ docker stop $(docker ps -q)
 
 #generate secret key
 SECRET_KEY="$(docker run oadam/proprio 2>&1|sed -n '23,23 p')"
-echo will use secret key: $SECRET_KEY
+echo will use secret key: "$SECRET_KEY"
 
 # start the app
 echo -e "\n\nproprio will start now. Once started it will be available on http://192.168.50.60/"
-docker run -p 80:8000 -e SECRET_KEY=$SECRET_KEY -v /proprio-data:/data oadam/proprio
+docker run -p 80:8000 -e SECRET_KEY="$SECRET_KEY" -v /proprio-data:/data oadam/proprio
 
